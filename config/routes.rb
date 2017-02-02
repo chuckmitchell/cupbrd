ad1e00053e689a4b718b792f361494bbbe8e5540
Charles Mitchell <cmitchell@kanayo.com>
2017-02-01 20:25:06 -0400

    name api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@
 Rails.application.routes.draw do
   scope module: 'api' do
     namespace :v1 do
-      resources :users
-      resources :foods
-      resources :items
-      resources :units
+      resources :users, as: 'users';
+      resources :foods, as: 'foods'
+      resources :items, as: 'items'
+      resources :units, as: 'units'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
